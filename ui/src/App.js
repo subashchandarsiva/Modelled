35b109c84730912c28ffc83ea81b29f394b7fc33
subashchandarsiva <subashchandarsiva@gmail.com>
2020-07-09 09:19:25 +0100

    buttoned with Bootstrap

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary" className="mr-2">
+          Primary
+        </Button>
       </header>
     </div>
   );
